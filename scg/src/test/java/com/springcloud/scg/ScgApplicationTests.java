76316047001a58c4971692d611f010d076b12ea2
yeahdy <yejine98@naver.com>
2024-05-25 22:03:21 +0900

    init: add scg

---
+++ scg/src/test/java/com/springcloud/scg/ScgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcloud.scg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
