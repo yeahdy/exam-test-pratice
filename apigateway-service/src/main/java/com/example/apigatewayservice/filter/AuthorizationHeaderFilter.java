de54ffcf59fe5e8ed418a231974b7262d0855dfc
yeahdy <yejine98@naver.com>
2024-06-04 14:08:06 +0900

    Feat: cloud config 연동
    - yml 환경설정
    - 회원 서비스 actutator api 추가

--- apigateway-service/src/main/java/com/example/apigatewayservice/filter/AuthorizationHeaderFilter.java
+++ apigateway-service/src/main/java/com/example/apigatewayservice/filter/AuthorizationHeaderFilter.java
@@ -12,6 +12,7 @@ import javax.crypto.spec.SecretKeySpec;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.http.HttpHeaders;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.HttpStatus;
@@ -23,6 +24,7 @@ import reactor.core.publisher.Mono;
 
 @Component
 @Slf4j
+@RefreshScope
 public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<AuthorizationHeaderFilter.Config> {
 
     @Value("${jwt.token.secret}")
