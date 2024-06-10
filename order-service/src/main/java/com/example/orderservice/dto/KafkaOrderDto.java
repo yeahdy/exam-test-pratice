8fc50d5bc7311590d049b4a6a760fcf1180aebaf
yeahdy <yejine98@naver.com>
2024-06-10 16:46:37 +0900

    Feat: Kafka source connect dto 생성

---
+++ order-service/src/main/java/com/example/orderservice/dto/KafkaOrderDto.java
@@ -0,0 +1,12 @@
+package com.example.orderservice.dto;
+
+import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class KafkaOrderDto implements Serializable {
+    private Schema schema;
+    private Payload payload;
+}
