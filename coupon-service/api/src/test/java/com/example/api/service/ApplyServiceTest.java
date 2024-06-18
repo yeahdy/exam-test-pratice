ce78030da36284ccbd163a5a46e48dc2871713c4
yeahdy <yejine98@naver.com>
2024-06-18 14:08:42 +0900

    Test: 쿠폰 여러번 응모하기 테스트 코드 작성

--- coupon-service/api/src/test/java/com/example/api/service/ApplyServiceTest.java
+++ coupon-service/api/src/test/java/com/example/api/service/ApplyServiceTest.java
@@ -3,6 +3,10 @@ package com.example.api.service;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.example.api.repository.CouponRepository;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,6 +21,7 @@ public class ApplyServiceTest {
     private CouponRepository couponRepository;
 
     @Test
+    @DisplayName("쿠폰 한번만 응모하기")
     public void apply_one_coupon_test(){
         //given
         applyService.apply(1L);
@@ -24,6 +29,29 @@ public class ApplyServiceTest {
         long count = couponRepository.count();
         //then
         assertThat(count).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("쿠폰 여러분 응모하기")
+    public void apply_multiple_coupon_test() throws InterruptedException {
+        int threadCount = 1000;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i<threadCount; i++) {
+            long userId = i;
+            executorService.submit(() -> {
+                try{
+                    applyService.apply(userId);
+                }finally {
+                    latch.countDown();
+                }
+            });
+        }
+        latch.await();
+
+        long count = couponRepository.count();
+        assertThat(count).isEqualTo(100);
     }
 
 }
