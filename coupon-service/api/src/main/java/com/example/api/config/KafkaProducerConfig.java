dc3ac5176f7bacbc5ece30d908b8023369aa3339
yeahdy <yejine98@naver.com>
2024-06-24 23:11:29 +0900

    Feat: kafka 설정파일 cloud-config 연동

--- coupon-service/api/src/main/java/com/example/api/config/KafkaProducerConfig.java
+++ coupon-service/api/src/main/java/com/example/api/config/KafkaProducerConfig.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -13,11 +14,14 @@ import org.springframework.kafka.core.ProducerFactory;
 @Configuration
 public class KafkaProducerConfig {
 
+    @Value("${spring.kafka.producer.bootstrap-servers}")
+    private String bootstrapServers;
+
     @Bean
     public ProducerFactory<String, String> producerFactory(){
         Map<String, Object> config = new HashMap<>();
 
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         return new DefaultKafkaProducerFactory<>(config);
--- coupon-service/api/src/main/java/com/example/api/producer/CouponCreateProducer.java
+++ coupon-service/api/src/main/java/com/example/api/producer/CouponCreateProducer.java
@@ -1,6 +1,7 @@
 package com.example.api.producer;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
@@ -8,10 +9,13 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class CouponCreateProducer {
 
+    @Value("${spring.kafka.topics.user-to-coupon}")
+    private String userCouponTopic;
+
     private final KafkaTemplate<String, String> kafkaTemplate;
 
     public void createCoupon(String userId){
-        kafkaTemplate.send("coupon-create", userId);
+        kafkaTemplate.send(userCouponTopic, userId);
     }
 
 }
