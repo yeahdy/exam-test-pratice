907cca8ad753bb2173c98c81ed95a1ed3e0fc1cd
yeahdy <yejine98@naver.com>
2024-06-30 21:17:37 +0900

    Feat: mysql testContainer 환경셋팅
    - IntegrationTest 클래스 추가

--- first-service/src/test/java/com/example/firstservice/FirstServiceApplicationTests.java
+++ first-service/src/test/java/com/example/firstservice/FirstServiceApplicationTests.java
@@ -3,8 +3,7 @@ package com.example.firstservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class FirstServiceApplicationTests {
+class FirstServiceApplicationTests extends IntegrationTest {
 
     @Test
     void contextLoads() {
---
+++ first-service/src/test/java/com/example/firstservice/IntegrationTest.java
@@ -0,0 +1,59 @@
+package com.example.firstservice;
+
+import jakarta.transaction.Transactional;
+import java.io.File;
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.Ignore;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.testcontainers.containers.DockerComposeContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+
+@SpringBootTest
+@Ignore //상속용으로 만든 부모클래스로 Test가 실행되지 않아도 됨
+@Transactional
+@ContextConfiguration(initializers = IntegrationTest.IntegrationTestInitializer.class)
+public class IntegrationTest {
+
+    static DockerComposeContainer rdbms;
+
+    static {
+        rdbms = new DockerComposeContainer(new File("infra/test/docker-compose.yaml"))
+                .withExposedService(
+                        "local-db",
+                        3306,
+                        Wait.forLogMessage(".*ready for connections.*", 1)
+                                .withStartupTimeout(Duration.ofSeconds(300))
+                )
+                .withExposedService(
+                        "local-db-migrate",
+                        0,
+                        Wait.forLogMessage("(.*Successfully applied.*)|(.*Successfully validated.*)",1)
+                                .withStartupTimeout(Duration.ofSeconds(300))
+                );
+
+        rdbms.start();
+    }
+
+    //테스트가 동작할때 application.yaml 파일을 초기화
+    static class IntegrationTestInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        @Override
+        public void initialize(ConfigurableApplicationContext applicationContext) {
+            Map<String, String> properties = new HashMap<>();
+
+            //docker container에 올라간 DB의 host와 port 가져오기
+            var rdbmsHost = rdbms.getServiceHost("local-db", 3306);
+            var rdbmsPort = rdbms.getServicePort("local-db", 3306);
+
+            //database url을 동적으로 설정해서 테스트용 DB로 연결
+            properties.put("spring.datasource.url", "jdbc:mysql://"+rdbmsHost+":"+rdbmsPort+"/score");
+            TestPropertyValues.of(properties)
+                    .applyTo(applicationContext);
+        }
+    }
+}
