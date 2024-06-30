945fb15033841b7c2d21d0b9b4289a6780f0249c
yeahdy <yejine98@naver.com>
2024-06-30 14:55:47 +0900

    Test: 성적 저장 로직 검증 테스트코드 작성

--- first-service/src/test/java/com/example/firstservice/service/StudentScoreServiceTest.java
+++ first-service/src/test/java/com/example/firstservice/service/StudentScoreServiceTest.java
@@ -11,7 +11,7 @@ import org.mockito.Mockito;
 public class StudentScoreServiceTest {
 
     @Test
-    @DisplayName("시험 점수 저장 mocking 테스트")
+    @DisplayName("시험 점수 저장하기")
     void save_score_mock_test(){
         //given
         StudentScoreService studentScoreService = new StudentScoreService(
@@ -30,6 +30,66 @@ public class StudentScoreServiceTest {
         studentScoreService.saveScore(
                 new StudentScoreDto(exam, studentName, korScore, englishScore, mathScore)
         );
+    }
+
+    @Test
+    @DisplayName("시험 평균점수 60점 이상일 경우")
+    void save_exam_passed_test(){
+        //given
+        StudentScoreRepository studentScoreRepository= Mockito.mock(StudentScoreRepository.class);
+        PassedExamRepository passedExamRepository = Mockito.mock(PassedExamRepository.class);
+        FailedExamRepository failedExamRepository = Mockito.mock(FailedExamRepository.class);
+        StudentScoreService studentScoreService = new StudentScoreService(
+                studentScoreRepository,
+                passedExamRepository,
+                failedExamRepository
+        );
+
+        String exam = "중간고사";
+        String studentName = "ellie";
+        Integer korScore = 78;
+        Integer englishScore = 93;
+        Integer mathScore = 83;
+
+        //when
+        studentScoreService.saveScore(
+                new StudentScoreDto(exam, studentName, korScore, englishScore, mathScore)
+        );
+
+        //then
+        //passedExam 테이블에는 저장되고, failedExam 테이블에는 저장이 되지 않아야 한다.
+        Mockito.verify(passedExamRepository, Mockito.times(1)).save(Mockito.any());
+        Mockito.verify(failedExamRepository, Mockito.times(0)).save(Mockito.any());
+    }
+
+    @Test
+    @DisplayName("시험 평균점수 60점 미만일 경우")
+    void save_exam_failed_test(){
+        //given
+        StudentScoreRepository studentScoreRepository= Mockito.mock(StudentScoreRepository.class);
+        PassedExamRepository passedExamRepository = Mockito.mock(PassedExamRepository.class);
+        FailedExamRepository failedExamRepository = Mockito.mock(FailedExamRepository.class);
+        StudentScoreService studentScoreService = new StudentScoreService(
+                studentScoreRepository,
+                passedExamRepository,
+                failedExamRepository
+        );
+
+        String exam = "중간고사";
+        String studentName = "ellie";
+        Integer korScore = 48;
+        Integer englishScore = 58;
+        Integer mathScore = 35;
+
+        //when
+        studentScoreService.saveScore(
+                new StudentScoreDto(exam, studentName, korScore, englishScore, mathScore)
+        );
+
+        //then
+        //failedExam 테이블에는 저장되고, passedExam 테이블에는 저장이 되지 않아야 한다.
+        Mockito.verify(passedExamRepository, Mockito.times(0)).save(Mockito.any());
+        Mockito.verify(failedExamRepository, Mockito.times(1)).save(Mockito.any());
     }
 
 }
