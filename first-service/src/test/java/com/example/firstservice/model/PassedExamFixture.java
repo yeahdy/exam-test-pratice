5b28c386c26acda4dd27af858e24bb8c04283821
yeahdy <yejine98@naver.com>
2024-06-30 20:09:55 +0900

    Refactor: StudentScore, FailedExam, PassedExam 테스트 데이터 빌더 생성

---
+++ first-service/src/test/java/com/example/firstservice/model/PassedExamFixture.java
@@ -0,0 +1,30 @@
+package com.example.firstservice.model;
+
+import com.example.firstservice.domain.PassedExamEntity;
+import com.example.firstservice.domain.StudentScoreEntity;
+import com.example.firstservice.utils.MyCalculator;
+
+public class PassedExamFixture {
+
+    public static PassedExamEntity passed(StudentScoreEntity studentScore) {
+        return PassedExamEntity.builder()
+                .exam(studentScore.getExam())
+                .studentName(studentScore.getStudentName())
+                .avgScore(new MyCalculator(0.0)
+                        .add(studentScore.getKorScore().doubleValue())
+                        .add(studentScore.getEnglishScore().doubleValue())
+                        .add(studentScore.getMathScore().doubleValue())
+                        .divid(3.0)
+                        .getResult())
+                .build();
+    }
+
+    public static PassedExamEntity passed(String exam, String studentName) {
+        return PassedExamEntity.builder()
+                .exam(exam)
+                .studentName(studentName)
+                .avgScore(94.3)
+                .build();
+    }
+
+}
--- first-service/src/test/java/com/example/firstservice/service/StudentScoreServiceTest.java
+++ first-service/src/test/java/com/example/firstservice/service/StudentScoreServiceTest.java
@@ -2,7 +2,11 @@ package com.example.firstservice.service;
 
 import com.example.firstservice.domain.FailedExamEntity;
 import com.example.firstservice.domain.PassedExamEntity;
+import com.example.firstservice.domain.StudentScoreEntity;
 import com.example.firstservice.dto.StudentScoreDto;
+import com.example.firstservice.model.FailedExamFixture;
+import com.example.firstservice.model.PassedExamFixture;
+import com.example.firstservice.model.StudentScoreData;
 import com.example.firstservice.repository.FailedExamRepository;
 import com.example.firstservice.repository.PassedExamRepository;
 import com.example.firstservice.repository.StudentScoreRepository;
@@ -56,28 +60,18 @@ public class StudentScoreServiceTest {
     @DisplayName("시험 평균점수 60점 이상일 경우")
     void save_exam_passed_test() {
         //given
-        String exam = "중간고사";
-        String studentName = "ellie";
-        Integer korScore = 78;
-        Integer englishScore = 93;
-        Integer mathScore = 83;
-
-        PassedExamEntity expectPassedExamEntity = PassedExamEntity.builder()
-                .exam(exam)
-                .studentName(studentName)
-                .avgScore(new MyCalculator(0.0)
-                        .add(korScore.doubleValue())
-                        .add(englishScore.doubleValue())
-                        .add(mathScore.doubleValue())
-                        .divid(3.0)
-                        .getResult())
-                .build();
+        StudentScoreEntity studentScore = StudentScoreData.passed().build();
+        PassedExamEntity expectPassedExamEntity = PassedExamFixture.passed(studentScore);
 
         //when
         //ArgumentCaptor: 메서드에 전달된 인자를 캡쳐해서 전달한 인자값이 제대로 로직에서 작동하는지 검증
         ArgumentCaptor<PassedExamEntity> passedExamEntityCaptor = ArgumentCaptor.forClass(PassedExamEntity.class);
         studentScoreService.saveScore(
-                new StudentScoreDto(exam, studentName, korScore, englishScore, mathScore)
+                new StudentScoreDto(studentScore.getExam(),
+                        studentScore.getStudentName(),
+                        studentScore.getKorScore(),
+                        studentScore.getEnglishScore(),
+                        studentScore.getMathScore())
         );
 
         //then
@@ -95,27 +89,18 @@ public class StudentScoreServiceTest {
     @DisplayName("시험 평균점수 60점 미만일 경우")
     void save_exam_failed_test() {
         //given
-        String exam = "중간고사";
-        String studentName = "ellie";
-        Integer korScore = 48;
-        Integer englishScore = 58;
-        Integer mathScore = 35;
-
-        FailedExamEntity expectFailedExamEntity = FailedExamEntity.builder()
-                .exam(exam)
-                .studentName(studentName)
-                .avgScore(new MyCalculator(0.0)
-                        .add(korScore.doubleValue())
-                        .add(englishScore.doubleValue())
-                        .add(mathScore.doubleValue())
-                        .divid(3.0)
-                        .getResult())
-                .build();
+        StudentScoreEntity studentScore = StudentScoreData.failed()
+                .exam("FINAL").build();
+        FailedExamEntity expectFailedExamEntity = FailedExamFixture.failed(studentScore);
 
         //when
         ArgumentCaptor<FailedExamEntity> failedExamEntityCaptor = ArgumentCaptor.forClass(FailedExamEntity.class);
         studentScoreService.saveScore(
-                new StudentScoreDto(exam, studentName, korScore, englishScore, mathScore)
+                new StudentScoreDto(studentScore.getExam(),
+                        studentScore.getStudentName(),
+                        studentScore.getKorScore(),
+                        studentScore.getEnglishScore(),
+                        studentScore.getMathScore())
         );
 
         //then
@@ -133,16 +118,14 @@ public class StudentScoreServiceTest {
     @DisplayName("특정 시험에 대한 합격자 명단 리스트 조회하기")
     void get_passed_exam_students_list_test() {
         //given
-        PassedExamEntity finalExam1 = PassedExamEntity.builder().exam("기말고사").studentName("ellie").avgScore(83.6)
-                .build();
-        PassedExamEntity finalExam2 = PassedExamEntity.builder().exam("기말고사").studentName("lie").avgScore(63.9).build();
-        PassedExamEntity middleExam = PassedExamEntity.builder().exam("중간고사").studentName("elephant").avgScore(77.3)
-                .build();
+        String exam = "기말고사";
+        PassedExamEntity finalExam1 = PassedExamFixture.passed(exam,"ellie");
+        PassedExamEntity finalExam2 = PassedExamFixture.passed(exam,"lie");
+        PassedExamEntity middleExam = PassedExamFixture.passed("중간고사","elephant");
         Mockito.when(passedExamRepository.findAll()).thenReturn(
                 List.of(finalExam1, finalExam2, middleExam));
 
         //when
-        String exam = "기말고사";
         var expectResponse = List.of(finalExam1, finalExam2)
                 .stream()
                 .map(passedExam -> new ResponsePassedExamStudent(passedExam.getStudentName(), passedExam.getAvgScore()))
@@ -157,16 +140,14 @@ public class StudentScoreServiceTest {
     @DisplayName("특정 시험에 대한 불합격자 명단 리스트 조회하기")
     void get_failed_exam_students_list_test() {
         //given
-        FailedExamEntity finalExam1 = FailedExamEntity.builder().exam("기말고사").studentName("ellie").avgScore(56.6)
-                .build();
-        FailedExamEntity finalExam2 = FailedExamEntity.builder().exam("기말고사").studentName("lie").avgScore(33.9).build();
-        FailedExamEntity middleExam = FailedExamEntity.builder().exam("중간고사").studentName("elephant").avgScore(47.3)
-                .build();
+        String exam = "기말고사";
+        FailedExamEntity finalExam1 = FailedExamFixture.failed(exam, "ellie");
+        FailedExamEntity finalExam2 = FailedExamFixture.failed(exam, "lie"); ;
+        FailedExamEntity middleExam = FailedExamFixture.failed("중간고사", "elephant");
         Mockito.when(failedExamRepository.findAll()).thenReturn(
                 List.of(finalExam1, finalExam2, middleExam));
 
         //when
-        String exam = "기말고사";
         var expectResponse = List.of(finalExam1, finalExam2)
                 .stream()
                 .map(passedExam -> new ResponseFailedExamStudent(passedExam.getStudentName(), passedExam.getAvgScore()))
